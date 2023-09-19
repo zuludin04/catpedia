33e9c0664670044da8f11abf56775cc7d9b947d2
zuludin <zmauludin04@gmail.com>
2023-09-20 05:10:47 +0700

    Initial Commit

---
+++ Catpedia/CatpediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatpediaApp.swift
+//  Catpedia
+//
+//  Created by zuludin on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CatpediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
